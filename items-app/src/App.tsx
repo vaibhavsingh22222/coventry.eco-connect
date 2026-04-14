5bfd67ce5b824ef2a2a7d51066ec9875c9f33d38
vaibhavsingh22222 <sharmav33@coventry.ac.uk>
2026-04-14 10:40:00 +0000

    commit 7

--- items-app/src/App.tsx
+++ items-app/src/App.tsx
@@ -1,22 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Home from "./pages/Home"
-import Items from "./pages/Items"
-import Item from "./pages/Item"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Items from "./pages/Items";
+import Item from "./pages/Item";
+
 function App() {
-return (
-<BrowserRouter>
-<Header />
-<main className="p-6">
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/items" element={<Items />} />
-<Route path="/items/:id" element={<Item />} />
-</Routes>
-</main>
-<Footer />
-</BrowserRouter>
-)
+  return (
+    <BrowserRouter>
+      
+      {/* HEADER */}
+      <Header />
+
+      {/* MAIN CONTENT */}
+      <main className="min-h-screen bg-slate-50">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/items/:id" element={<Item />} />
+        </Routes>
+      </main>
+
+      {/* FOOTER */}
+      <Footer />
+
+    </BrowserRouter>
+  );
 }
-export default App
+
+export default App;
